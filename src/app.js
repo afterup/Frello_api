1bdccfdb3c4eb9342c42a62f56c880bd902285ce
fora <uptohai@gmail.com>
2020-01-09 12:13:16 +0900

    Refactor project structure

--- src/app.js
+++ src/app.js
@@ -1,24 +1,39 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const cors = require('cors');
-const logger = require('morgan');
+
+
+// Create global app object
+const app = express();
+
+// express config defaults
+app.use(require('morgan')('dev'));
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+app.use(cors());
 
 const config = require('./config/config');
 const { sequelize, User, Board, Favorite, List, Card } = require('./models');
 
-const app = express();
+app.use(require('./routes/index'));
 
-app.use(bodyParser.urlencoded({ extended: false }));
-app.use(bodyParser.json());
-app.use(logger('dev'));
-app.use(cors());
+/// error handler
+app.use(function(req, res, next) {
+    const err = new Error('Not Found');
+    err.status = 404;
+    next(err);
+});
 
-app.set('jwt-secret', config.authentication.jwtSecret);
+app.use(function(err, req, res, next) {
+    res.status(err.status || 500);
+    res.json({
+        errors: {
+            message: err.message,
+            error: {}
+        }
+    });
+});
 
-require('./routes/auth')(app);
-require('./routes/board')(app);
-require('./routes/card')(app);
-require('./routes/list')(app);
 
 User.hasMany(Board, { foreignKey: { name: 'user_id', allowNull: false }, sourceKey: 'user_id', onDelete: 'cascade' });
 Board.belongsTo(User, { foreignKey: 'user_id', targetKey: 'user_id' });
