4703b6531f75e18a6e7de30bb19278dd47834c82
fora <uptohai@gmail.com>
2020-01-09 20:27:37 +0900

    Refactor board update, delete function

--- src/routes/board.js
+++ src/routes/board.js
@@ -5,55 +5,97 @@ const auth = require('../middlewares/auth');
 
 // routes '/board' 
 
-router.post('/', auth.required, function(req, res, next) {
-    console.log(req.payload);
-    Board.create({
-        user_id: req.payload.user_id,
-        title: req.body.board.title,
-        background: req.body.board.title
-    }).then((result) => {
-        res.status(201).send(result);
-    }).catch((err) => {
-        res.status(500).send({ error: { body: err.message } });
-    });
-});
+router.post('/', auth.required, 
+    function(req, res, next) {
+        console.log(req.payload);
+        Board.create({
+            user_id: req.payload.user_id,
+            title: req.body.board.title,
+            background: req.body.board.title
+        }).then((result) => {
+            res.status(201).send(result);
+        }).catch((err) => {
+            res.status(500).send({ error: { body: err.message } });
+        });
+    }
+);
 
-router.get('/', auth.required, function(req, res) {
-    console.log(req.payload);
-    Board.findAll({ 
-        where: { user_id: req.payload.user_id },
-        order: [['updatedAt', 'DESC']]
-    })
-        .then((boards) => {
-            res.status(200).send({
-                boards: boards
-            });
+router.get('/', auth.required, 
+    function(req, res) {
+        console.log(req.payload);
+        Board.findAll({ 
+            where: { user_id: req.payload.user_id },
+            order: [['updatedAt', 'DESC']]
         })
-        .catch(err => {
-            res.status(500).send({ error: { body: err.message } });
-        });
-});
-
-router.get('/:id', function(req, res) {
-    Board.findOne({
-        where: { board_id: req.params.id },
-        include: [{ 
-            model: List, 
-            include: [{
-                model: Card,
-                order: [['position', 'ASC']]
-            }],
-            order: [['position', 'ASC']]
-        }]
-    })
-        .then((board) => {
-            res.status(200).send({
-                board: board
+            .then((boards) => {
+                res.status(200).send({
+                    boards: boards
+                });
+            })
+            .catch(err => {
+                res.status(500).send({ error: { body: err.message } });
             });
-        }).catch(err => {
+    }
+);
+
+router.get('/:id', 
+    function(req, res) {
+        Board.findOne({
+            where: { board_id: req.params.id },
+            include: [{ 
+                model: List, 
+                include: [{
+                    model: Card,
+                    order: [['position', 'ASC']]
+                }],
+                order: [['position', 'ASC']]
+            }]
+        })
+            .then((board) => {
+                res.status(200).send({
+                    board: board
+                });
+            }).catch(err => {
+                res.status(500).send({ error: { body: err.message } });
+            });
+    }
+);
+
+router.put('/:id', auth.required,
+    async function(req, res) {
+        try{
+            if(req.body.board.user_id.toString() === req.payload.user_id.toString()) {
+                const board = await Board.findOne({ where: { board_id: req.params.id } });
+                if(!board) {
+                    return res.status(406).send({ error: { body: 'board_id not exist' } });
+                }
+                await Board.update(
+                    req.body.board, 
+                    { where: { board_id: req.params.id } }
+                );
+                res.status(200).send({ message: 'success' });
+            }else {
+                return res.status(403).send({ error: { body: 'Forbidden' } });
+            }
+        }catch(err) {
             res.status(500).send({ error: { body: err.message } });
-        });
-});
+        }
+    }
+);
+
+router.delete('/:id', auth.required,
+    function(req, res) {
+        if(req.body.user_id.toString() === req.payload.user_id.toString()) {
+            Board.destroy({
+                where: { board_id: req.params.id }
+            }).then(() => {
+                res.status(200).send({ message: 'success' });
+            });
+        }else{
+            res.status(403).send({ error: { body: 'Forbidden' } });
+        }
+    }
+);
 
 
 module.exports = router;
