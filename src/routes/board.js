2472ec350ca8f1c11aa2f3359ca236e4e17ab7ea
fora <uptohai@gmail.com>
2020-01-11 01:54:37 +0900

    Feat: add board favorite create, delete router

--- src/routes/board.js
+++ src/routes/board.js
@@ -1,5 +1,6 @@
-const { Board, List, Card } = require('../models');
+const { Board, List, Card, Favorite } = require('../models');
 const sequelize = require('sequelize');
+const Op = sequelize.Op;
 const router = require('express').Router();
 const auth = require('../middlewares/auth');
 
@@ -93,6 +94,45 @@ router.delete('/:id', auth.required,
             });
         }else{
             res.status(403).send({ error: { message: 'Forbidden' } });
+        }
+    }
+);
+
+router.post('/:id/favorite', auth.required,
+    async function(req, res) {
+        try{
+            const board = await Board.findOne({ where: { board_id: req.params.id } });
+            if(!board) {
+                return res.status(403).send({ error: { message: 'Not Found Board' } });
+            }
+            const favorite = await Favorite.create({
+                user_id: req.payload.user_id,
+                board_id: req.params.id
+            });
+            res.status(201).send(favorite);
+        }catch(err) {
+            res.status(500).send({ error: { message: err.message } });
+        }
+    }
+);
+
+router.delete('/:id/favorite', auth.required,
+    async function(req, res) {
+        try{
+            const favoriteId = await Favorite.findOne({
+                where: {
+                    [Op.or]: [{ board_id: req.params.id }, { user_id: req.payload.user_id }]
+                }
+            });
+    
+            if(!favoriteId) {
+                return res.status(406).send({ error: { message: 'favorite not exist' } });
+            }
+    
+            await Favorite.destroy({ where: { favorite_id: favoriteId.favorite_id } });
+            res.status(200).send({ message: 'success' });
+        }catch(err) {
+            res.status(500).send({ error: { message: err.message } });
         }
     }
 );
--- src/routes/index.js
+++ src/routes/index.js
@@ -1,9 +1,13 @@
 const router = require('express').Router();
 const user = require('./user');
 const board = require('./board');
+const list = require('./list');
+const card = require('./card');
 
 router.use('/', user);
 router.use('/board', board);
+router.use('/list', list);
+router.use('/card', card);
 
 router.use(function(err, req, res, next) {
     if(err.name === 'ValidationError') {
