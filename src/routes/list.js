e77261983ca5b65be51236b2bf64d27fe3642d8f
fora <uptohai@gmail.com>
2020-01-18 21:55:17 +0900

    Refactor: list create, position update

--- src/routes/list.js
+++ src/routes/list.js
@@ -1,19 +1,40 @@
 const { List } = require('../models');
 const router = require('express').Router();
+const sequelize = require('sequelize');
+const Op = sequelize.Op;
 const auth = require('../middlewares/auth');
 
 // routes '/list'
 
 router.post('/', auth.required,
-    function(req, res) {
-        console.log(req.payload);
-        const reqList = req.body.list;
+    async function(req, res) {
+        /* eslint-disable camelcase */
+        const { board_id, title } = req.body.list;
+        let position;
+
+        const maximumPosition = await List.findOne({
+            limit: 1,
+            where: {
+                board_id: board_id
+            },
+            order: [['position', 'desc']]
+        });
+
+        console.log(maximumPosition);
+
+        if(maximumPosition) {
+            position = maximumPosition.position + 65535;
+        }else {
+            position = 65535;
+        }
+
+        console.log(position);
 
         List.create({
             user_id: req.payload.user_id,
-            board_id: reqList.board_id,
-            title: reqList.title,
-            position: reqList.position
+            board_id: board_id,
+            title: title,
+            position: position
         }).then((result) => {
             res.status(201).send({ list: result });
         }).catch(err => {
@@ -34,13 +55,40 @@ router.put('/:id', auth.required,
                 return res.status(406).send({ error: { message: 'list_id not exist' } });
             }
 
+            const { title, bothList } = req.body.list;
+
             if(listUserId.user_id === req.payload.user_id) {
-                const list = await List.update(
-                    req.body.list,
-                    { where: { list_id: req.params.id } }
-                );
-                console.log(list);
-                return res.status(200).send({ message: 'success' });
+                if(title) {
+                    await List.update(
+                        { title },
+                        { where: { list_id: req.params.id } }
+                    );
+                }else if(bothList) {
+                    const { leftListPosition, rightListPosition } = bothList;
+                    let position;
+
+                    if(leftListPosition) {
+                        if(rightListPosition) {
+                            // 두 포지션 사이의 난수 생성
+                            position = Math.floor(Math.random() * (rightListPosition - leftListPosition)) + leftListPosition;
+                        }else {
+                            position = leftListPosition + Math.floor(Math.random() * 1000);
+                        }
+                    }else {
+                        position = rightListPosition - Math.floor(Math.random() * 1000);
+                    }
+
+                    await List.update(
+                        { position },
+                        { where: { list_id: req.params.id } }
+                    );
+                }
+
+                const list = await List.findOne({
+                    where: { list_id: req.params.id }
+                });
+
+                return res.status(200).send({ message: 'success', list: list });
             }else {
                 return res.status(403).send({ error: { message: 'Forbidden' } });
             }
